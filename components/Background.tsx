14e110cdd48118bcf62229c53f60a39a1773e737
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-09-20 15:16:32 +0545

    Background UI colorr fixed

---
+++ components/Background.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Background() {
+    return (
+        <div className='fixed -z-10 h-screen w-full bg-zinc-800 flex items-center justify-center select-none'>
+
+            <span className='font-extrabold  text-6xl sm:text-9xl text-zinc-900'>Docs To Do.</span>
+
+        </div>
+    )
+}
+
+export default Background
