2bc96af15550957d012d44f73c986854b45d49da
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-09-20 15:21:59 +0545

    Add: Background component for layout visuals

--- components/Background.tsx
+++ components/Background.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 function Background() {
     return (
+        // Background css for layout visuals
         <div className='fixed -z-10 h-screen w-full bg-zinc-800 flex items-center justify-center select-none'>
 
             <span className='font-extrabold  text-6xl sm:text-9xl text-zinc-900'>Docs To Do.</span>
