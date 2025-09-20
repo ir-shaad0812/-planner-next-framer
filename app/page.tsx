2fb1b1c391d820be1b77a00a0ce40e712629c9b6
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-09-20 11:12:55 +0545

    Add base page structure with comlete

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Background from "@/components/Background";
+import Foreground from "@/components/Foreground";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+
+      <Background />
+
+      <Foreground />
+
+    </>
+  );
+}
