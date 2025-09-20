b1f3b95a0ed0ed720a6aabe0502f9832cb30f052
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-09-20 11:19:40 +0545

    Add mock or static data for app

---
+++ data/SampleData.ts
@@ -0,0 +1,27 @@
+export const sampleData = [
+
+    {
+        id: 1,
+        title: "My Usernames",
+        desc: "Github: xyz\nLinkedin: xy99 ",
+        isActive: true,
+        date: "07/07/2025"
+    },
+
+    {
+        id: 2,
+        title: "To-Do List",
+        desc: "Buy Vegies\nPay Bills\nCall Her",
+        isActive: false,
+        date: "17/07/2025"
+    },
+    
+     {
+        id: 3,
+        title: "To-Do List",
+        desc: "Research Day-01\nPay Bills\nCall Her\nBlock Her",
+        isActive: false,
+        date: "7/5/2025"
+    },
+
+];
